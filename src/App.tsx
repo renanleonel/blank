0d7a1f1dc07a17547036d8fc8b8c0fb4f8d3d798
renanleonel <renanleonelpro@gmail.com>
2025-10-11 17:47:06 -0300

    feat: create blank container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <main>blank.</main>;
+  return (
+    <div className='min-h-screen h-screen bg-white flex items-center justify-center p-8'>
+      <div className='w-full max-w-7xl h-full rounded-3xl shadow-2xl shadow-gray-400/20 border border-[#DCCFC0] bg-[#FAF9EE]'>
+        <div className='h-full p-8 flex items-center justify-center text-gray-500'></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
