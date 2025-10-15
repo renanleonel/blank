2db6b32b1d5bb10b14f896873a50fd50f1fba1a4
renanleonel <renanleonelpro@gmail.com>
2025-10-15 08:34:29 -0300

    refactor: create generic virtualized table

--- src/components/virtualized-table.tsx
+++ src/components/virtualized-table.tsx
@@ -1,112 +1,79 @@
-import { useListUsers } from '@/containers/users/queries/user-queries';
 import { cn } from '@/lib/utils';
-import { keepPreviousData } from '@tanstack/react-query';
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getSortedRowModel,
-  OnChangeFn,
-  Row,
-  SortingState,
-  useReactTable,
-} from '@tanstack/react-table';
+import { UseInfiniteQueryResult } from '@tanstack/react-query';
+import { ColumnDef, flexRender, getCoreRowModel, Row, useReactTable } from '@tanstack/react-table';
 import { useVirtualizer } from '@tanstack/react-virtual';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+
+type TablePagination = {
+  totalRowCount: number;
+  hasNextPage: boolean;
+  totalFetched: number;
+};
+
+export type TableData<T> = {
+  list: T[];
+  pagination: TablePagination;
+};
 
 type VirtualizedTableProps<T> = {
   columns: ColumnDef<T, any>[]; // eslint-disable-line @typescript-eslint/no-explicit-any
-  fetchSize?: number;
+  query: UseInfiniteQueryResult<TableData<T>>;
 };
 
-export function VirtualizedTable<T>({ columns, fetchSize = 50 }: VirtualizedTableProps<T>) {
-  // Reference to the scrolling element
+export function VirtualizedTable<T>({ query, columns }: VirtualizedTableProps<T>) {
   const tableContainerRef = useRef<HTMLDivElement>(null);
 
-  const [sorting, setSorting] = useState<SortingState>([]);
+  const { data, fetchNextPage, isFetching, isLoading } = query;
 
-  // React Query infinite query for paginated data
-  const listUsersQuery = useListUsers({
-    params: { fetchSize, sorting },
-    options: { placeholderData: keepPreviousData },
-  });
+  const flatData = data?.list ?? [];
+  const totalFetched = data?.pagination.totalFetched ?? 0;
+  const totalRowCount = data?.pagination.totalRowCount ?? 0;
 
-  const { data, fetchNextPage, isFetching, isLoading } = listUsersQuery;
-
-  // Extract data from the query result
-  const flatData = useMemo(() => data?.list ?? [], [data]);
-  const totalDBRowCount = data?.pagination?.totalRowCount ?? 0;
-  const totalFetched = data?.pagination?.totalFetched ?? 0;
-
-  // Called on scroll to fetch more data when user reaches bottom
   const fetchMoreOnBottomReached = useCallback(
     (containerRefElement?: HTMLDivElement | null) => {
       if (containerRefElement) {
         const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
-        // Once the user has scrolled within 500px of the bottom, fetch more data
+
         if (
           scrollHeight - scrollTop - clientHeight < 500 &&
           !isFetching &&
-          totalFetched < totalDBRowCount
+          totalFetched < totalRowCount
         ) {
           fetchNextPage();
         }
       }
     },
-    [fetchNextPage, isFetching, totalFetched, totalDBRowCount]
+    [fetchNextPage, isFetching, totalFetched, totalRowCount]
   );
-
-  // Check on mount and after fetch to see if table needs more data immediately
-  useEffect(() => {
-    fetchMoreOnBottomReached(tableContainerRef.current);
-  }, [fetchMoreOnBottomReached]);
 
   const table = useReactTable<T>({
     data: flatData as T[],
     columns,
-    state: {
-      sorting,
-    },
     getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    manualSorting: true,
     debugTable: true,
     columnResizeMode: 'onChange',
     enableColumnResizing: true,
     defaultColumn: {
-      minSize: 50,
       size: 100,
+      minSize: 50,
       maxSize: 500,
     },
   });
-
-  // Scroll to top of table when sorting changes
-  const handleSortingChange: OnChangeFn<SortingState> = (updater) => {
-    setSorting(updater);
-    if (table.getRowModel().rows.length > 0) {
-      rowVirtualizer.scrollToIndex?.(0);
-    }
-  };
-
-  // Update table options
-  table.setOptions((prev) => ({
-    ...prev,
-    onSortingChange: handleSortingChange,
-  }));
 
   const { rows } = table.getRowModel();
 
   const rowVirtualizer = useVirtualizer({
     count: rows.length,
-    estimateSize: () => 50, // Estimate row height for accurate scrollbar
+    estimateSize: () => 50,
     getScrollElement: () => tableContainerRef.current,
-    // Measure dynamic row height, except in Firefox
     measureElement:
       typeof window !== 'undefined' && navigator.userAgent.indexOf('Firefox') === -1
         ? (element) => element?.getBoundingClientRect().height
         : undefined,
     overscan: 5,
   });
+
+  useEffect(() => fetchMoreOnBottomReached(tableContainerRef.current), [fetchMoreOnBottomReached]);
 
   if (isLoading) {
     return (
@@ -118,42 +85,24 @@ export function VirtualizedTable<T>({ columns, fetchSize = 50 }: VirtualizedTabl
 
   return (
     <div className='rounded-lg border border-gray-200 h-full flex flex-col'>
-      {/* Fixed Header */}
       <div className='bg-gray-50 border-b border-gray-200'>
-        <table className='w-full' style={{ display: 'grid', width: '100%' }}>
-          <thead style={{ display: 'grid', width: '100%' }}>
+        <table className='w-full grid'>
+          <thead className='grid w-full'>
             {table.getHeaderGroups().map((headerGroup) => (
-              <tr
-                key={headerGroup.id}
-                style={{ display: 'flex', width: '100%' }}
-                className='bg-gray-50'>
+              <tr key={headerGroup.id} className='flex w-full bg-gray-50'>
                 {headerGroup.headers.map((header, index) => (
                   <th
                     key={header.id}
-                    className={`py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider ${
+                    className={cn(
+                      'py-2 flex text-left text-xs font-medium text-gray-500 uppercase tracking-wider',
                       index === headerGroup.headers.length - 1 ? 'px-0' : 'px-4'
-                    }`}
+                    )}
                     style={{
-                      display: 'flex',
-                      width: `${(header.getSize() / table.getTotalSize()) * 100}%`,
                       minWidth: header.column.columnDef.minSize,
                       maxWidth: header.column.columnDef.maxSize,
+                      width: `${(header.getSize() / table.getTotalSize()) * 100}%`,
                     }}>
-                    <div
-                      className={
-                        header.column.getCanSort()
-                          ? 'cursor-pointer select-none hover:text-gray-700'
-                          : ''
-                      }
-                      onClick={header.column.getToggleSortingHandler()}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(header.column.columnDef.header, header.getContext())}
-                      {{
-                        asc: ' 🔼',
-                        desc: ' 🔽',
-                      }[header.column.getIsSorted() as string] ?? null}
-                    </div>
+                    <div>{flexRender(header.column.columnDef.header, header.getContext())}</div>
                   </th>
                 ))}
               </tr>
@@ -162,48 +111,35 @@ export function VirtualizedTable<T>({ columns, fetchSize = 50 }: VirtualizedTabl
         </table>
       </div>
 
-      {/* Scrollable Body Container */}
       <div
-        className='flex-1 overflow-auto'
+        className='flex-1 overflow-auto h-[600px]'
         onScroll={(e) => fetchMoreOnBottomReached(e.currentTarget)}
-        ref={tableContainerRef}
-        style={{
-          height: '600px', // Fixed height for virtualization
-        }}>
-        <table className='w-full' style={{ display: 'grid', width: '100%' }}>
-          {/* Virtualized Body */}
+        ref={tableContainerRef}>
+        <table className='w-full grid'>
           <tbody
-            style={{
-              display: 'grid',
-              height: `${rowVirtualizer.getTotalSize()}px`,
-              position: 'relative',
-              width: '100%',
-            }}>
+            className='grid relative w-full'
+            style={{ height: `${rowVirtualizer.getTotalSize()}px` }}>
             {rowVirtualizer.getVirtualItems().map((virtualRow) => {
               const row = rows[virtualRow.index] as Row<T>;
+
               return (
                 <tr
                   data-index={virtualRow.index}
                   ref={(node) => rowVirtualizer.measureElement(node)}
                   key={row.id}
-                  className='hover:bg-gray-50'
-                  style={{
-                    display: 'flex',
-                    position: 'absolute',
-                    transform: `translateY(${virtualRow.start}px)`,
-                    width: '100%',
-                  }}>
+                  className='hover:bg-gray-50 flex absolute w-full'
+                  style={{ transform: `translateY(${virtualRow.start}px)` }}>
                   {row.getVisibleCells().map((cell, cellIndex) => (
                     <td
                       key={cell.id}
-                      className={`py-4 whitespace-nowrap text-sm text-gray-900 ${
+                      className={cn(
+                        'py-4 flex whitespace-now nowrap text-sm text-gray-900',
                         cellIndex === row.getVisibleCells().length - 1 ? 'px-0' : 'px-4'
-                      }`}
+                      )}
                       style={{
-                        display: 'flex',
-                        width: `${(cell.column.getSize() / table.getTotalSize()) * 100}%`,
                         minWidth: cell.column.columnDef.minSize,
                         maxWidth: cell.column.columnDef.maxSize,
+                        width: `${(cell.column.getSize() / table.getTotalSize()) * 100}%`,
                       }}>
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </td>
@@ -223,7 +159,7 @@ export function VirtualizedTable<T>({ columns, fetchSize = 50 }: VirtualizedTabl
         {isFetching && <span>Fetching more data...</span>}
 
         <span>
-          {flatData.length} of {totalDBRowCount} rows
+          {flatData.length} of {totalRowCount} rows
         </span>
       </div>
     </div>
--- src/containers/users/components/table/index.tsx
+++ src/containers/users/components/table/index.tsx
@@ -1,9 +1,18 @@
 import { VirtualizedTable } from '@/components/virtualized-table';
 import { useTableColumns } from '@/containers/users/hooks/use-table-columns';
+import { useListUsers } from '@/containers/users/queries/user-queries';
 import { User } from '@/lib/types/user';
+import { keepPreviousData } from '@tanstack/react-query';
+
+const FETCH_SIZE = 50;
 
 export function UsersTable() {
-  const { columns } = useTableColumns({ isLoading: false });
+  const listUsersQuery = useListUsers({
+    params: { fetchSize: FETCH_SIZE },
+    options: { placeholderData: keepPreviousData },
+  });
 
-  return <VirtualizedTable<User> columns={columns} />;
+  const { columns } = useTableColumns({ isLoading: listUsersQuery.isLoading });
+
+  return <VirtualizedTable<User> columns={columns} query={listUsersQuery} />;
 }
