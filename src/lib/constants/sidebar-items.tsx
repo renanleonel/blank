ddfd803d5e3bbdea97c60f2d92cd054eeef21353
renanleonel <renanleonelpro@gmail.com>
2025-10-22 08:31:33 -0300

    feat: add new sidebar items

--- src/lib/constants/sidebar-items.tsx
+++ src/lib/constants/sidebar-items.tsx
@@ -1,4 +1,11 @@
-import { FileText, Home, Settings } from 'lucide-react';
+import {
+  Brain,
+  FileText,
+  Home,
+  MapPin,
+  MessageSquare,
+  Settings,
+} from 'lucide-react';
 import { ReactNode } from 'react';
 
 export type SidebarItem = {
@@ -18,6 +25,21 @@ export const SIDEBAR_ITEMS: SidebarItem[] = [
     href: '/table',
     label: 'Table',
     icon: <FileText className="h-4 w-4" />,
+  },
+  {
+    href: '/geolocation',
+    label: 'Geolocation',
+    icon: <MapPin className="h-4 w-4" />,
+  },
+  {
+    href: '/chat',
+    label: 'Chat',
+    icon: <MessageSquare className="h-4 w-4" />,
+  },
+  {
+    href: '/ai',
+    label: 'AI Prompt',
+    icon: <Brain className="h-4 w-4" />,
   },
 ];
 
